48492a5ca0f42ae69f055851905c7be62e0b2f31
Deepak10225 <deepakpremad@gmail.com>
2025-05-12 22:29:26 +0530

    e

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
-    },
-  },
-  build: {
-    outDir: 'public/build',
-    emptyOutDir: true,
-  },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+    }),
+    vue(),
+  ],
 });
