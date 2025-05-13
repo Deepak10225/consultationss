86be38e31b49fa95fe3b2a9322572ef185d32a18
deepakgoswami <124884739+Deepak10225@users.noreply.github.com>
2025-05-13 13:18:33 +0530

    a

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <!-- <Notification />
-  <router-view /> -->
-  dgkaig
+  <Notification />
+  <router-view />
 </template>
 
 <script>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.js'],
-      refresh: true,
-    }),
-    vue(),
-  ],
-  build: {
-    outDir: 'public/build',
-    emptyOutDir: true,
-  }
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
 });
